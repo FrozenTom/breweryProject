758154d2c44f4ff8324585c17b6edf73da828bb1
FrozenTom <thomasnascenzi@yahoo.com>
2017-08-17 08:19:17 -0700

    Added git ignore

--- target/untappedBreweryProject-0.0.1-SNAPSHOT/javascripts/default.js
+++ target/untappedBreweryProject-0.0.1-SNAPSHOT/javascripts/default.js
@@ -1,33 +1,18 @@
-var main = function() {
-	console.log("main function called");
-	addButton();
-};
 
 
 var addButton = function(){
-	console.log("addBeer Clicked");
-	$("addBeer").click(function (){
-		console.log("addBeer Clicked");
-		$("beerRows").append(
-		`
-		<div class="form-group row">
-				<label for="example-text-input" class="col-2 col-form-label">Beer:</label>
-				<div class="form-group">
-					<select class="form-control" id="exampleSelect1">
-				 		<option>1</option>
-				     	<option>2</option>
-				     	<option>3</option>
-				      	<option>4</option>
-				      	<option>5</option>
-				    </select>
-				</div>
-		</div>
-		`
-		);
+	
+	$("#addBeer").click(function (){
+		numBeers++;
+		idString = `<div class="form-group row" id="` + numBeers + `">`;
+		$("#beerRows").append(idString + startString + middleString + endString);
+		
+		
 	});
-};	
-
-
-window.onload = function(){
-	main();
-}
+	$("#removeBeer").click(function (){
+		if(numBeers > 1 ){
+			$("#"+numBeers).remove();
+			numBeers--;
+		}
+	});
+};
--- target/untappedBreweryProject-0.0.1-SNAPSHOT/javascripts/readData.js
+++ target/untappedBreweryProject-0.0.1-SNAPSHOT/javascripts/readData.js
@@ -1,43 +1,146 @@
-var happy = function() {
-	
+var brw = [];
+var numBeers=1;
+var idString = `<div class="form-group row" id="` + numBeers + `">`
+var startString = 
+	`
+			<label for="example-text-input" class="col-2 col-form-label">Beer:</label>
+				<div class="form-group">
+					<select class="form-control beer" id="beerSelect">
+	`;
+var endString = `
+				    </select>
+				</div>
+		</div>
+`;
+function makeGraph(){
 	var ctx = document.getElementById("myChart");
-	var myChart = new Chart(ctx, {
-	    type: 'bar',
-	    data: {
-	        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-	        datasets: [{
-	            label: '# of Votes',
-	            data: [12, 19, 3, 5, 2, 3],
-	            backgroundColor: [
-	                'rgba(255, 99, 132, 0.2)',
-	                'rgba(54, 162, 235, 0.2)',
-	                'rgba(255, 206, 86, 0.2)',
-	                'rgba(75, 192, 192, 0.2)',
-	                'rgba(153, 102, 255, 0.2)',
-	                'rgba(255, 159, 64, 0.2)'
-	            ],
-	            borderColor: [
-	                'rgba(255,99,132,1)',
-	                'rgba(54, 162, 235, 1)',
-	                'rgba(255, 206, 86, 1)',
-	                'rgba(75, 192, 192, 1)',
-	                'rgba(153, 102, 255, 1)',
-	                'rgba(255, 159, 64, 1)'
-	            ],
-	            borderWidth: 1
-	        }]
-	    },
-	    options: {
-	        scales: {
-	            yAxes: [{
-	                ticks: {
-	                    beginAtZero:true
-	                }
-	            }]
-	        }
-	    }
+	var myChart;
+	$("#submit").click(function (){
+		var names = [];
+		var data = [];
+		var brwLoc = brw.map(function(x) {return x.name}).indexOf($("#brewerySelect option:selected" ).text());
+		$( ".beer option:selected" ).each(function( index ) {
+			var beerLoc = brw[brwLoc].beers.map(function(x) {return x.name}).indexOf($(this).text());
+			names.push($(this).text())
+			data.push(brw[brwLoc].beers[beerLoc].rating)
+			
+			console.log( index + ": " + $( this ).text() );
+		});
+		if(myChart){
+			myChart.destroy();
+			ctx = document.getElementById("myChart");
+		}
+		myChart = new Chart(ctx, {
+		    type: 'bar',
+		    data: {
+		        labels: names,
+		        datasets: [{
+		            label: 'Average Rating',
+		            data: data,
+		            borderWidth: 1
+		        }]
+		    },
+		    options: {
+		        scales: {
+		            yAxes: [{
+		                ticks: {
+		                    beginAtZero:true
+		                }
+		            }]
+		        }
+		    }
+		});
+	});
+}
+
+function setBreweryMenu(){
+
+	$("#brewerySelect").empty();
+	for(var i = 0; i < brw.length; i++){
+		$("#brewerySelect").append("<option>"+brw[i].name+"</option>")
+		
+	}
+	var loc = 0
+	var brewery = brw[loc];
+	//console.log(brewery)
+	$(".beer").empty();
+	$(".beer").append(brewery.beerOptions);
+	
+}
+function fillBeerMenu(){
+	//console.log("here");
+	$("#brewerySelect" ).change(function() {
+			//console.log("here2");
+			var loc = brw.map(function(x) {return x.name}).indexOf($("#brewerySelect option:selected" ).text());
+	    	var brewery = brw[loc];
+	    	$(".beer").empty();
+	    	$(".beer").append(brewery.beerOptions);
+	});
+}
+
+function addButton(){
+	
+	$("#addBeer").click(function (){
+		//console.log($("#brewerySelect option:selected" ).text());
+		var loc = brw.map(function(x) {return x.name}).indexOf($("#brewerySelect option:selected" ).text());
+		var brewery = brw[loc];
+		var middleString = brewery.beerOptions;
+		numBeers++;
+		idString = `<div class="form-group row" id="` + numBeers + `">`;
+		$("#beerRows").append(idString + startString + middleString + endString);
+		
+		
+	});
+	$("#removeBeer").click(function (){
+		if(numBeers > 1 ){
+			$("#"+numBeers).remove();
+			numBeers--;
+		}
 	});
 };
-window.onload = function(){
-	happy();
+
+
+
+
+
+
+function breweryList(breweryName,callback){
+	var breweries = [];
+	$.getJSON("/untappedBreweryProject/sampleJsonFiles/brewerySearch.json", function(data) {
+		
+		for(var i = 0; i < data.response.brewery.items.length; i++){
+			breweries[i] = [data.response.brewery.items[i].brewery.brewery_name, data.response.brewery.items[i].brewery.brewery_id];
+		}
+		callback(breweries);
+	});
 };
+
+function readBeerList(breweryID,callback){
+	var beers = [];
+	$.getJSON("/untappedBreweryProject/sampleJsonFiles/enegren.json", function(data) {
+		for(var i = 0; i < data.response.brewery.beer_list.items.length; i++){
+		  	beers[i] = new beer(data.response.brewery.beer_list.items[i].beer.beer_name, 
+		  			data.response.brewery.beer_list.items[i].beer.bid,
+		  			data.response.brewery.beer_list.items[i].beer.rating_score);
+		};
+		//console.log(JSON.stringify(beers))
+		callback(beers)
+	});
+};
+
+$(document).ready(function(){
+	function breweryCallback(breweries) {
+		//makeGraph(breweries[0].beerNames,breweries[0].beerRatings);
+		function beerCallback(beers) {
+			brw = [new brewery(breweries[0][0],breweries[0][1],beers)];
+			setBreweryMenu();
+			//makeGraph(brw[0].beerNames,brw[0].beerRatings);
+			//console.log(JSON.stringify(brw));
+		}
+		readBeerList(breweries[0][1], beerCallback)
+	}
+	breweryList("enegren",breweryCallback);
+	addButton();
+	fillBeerMenu();
+	makeGraph();
+});
